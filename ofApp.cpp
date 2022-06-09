011a3a571583babf12362319e288e7b5709de3ec
j-mayo <86545225+j-mayo@users.noreply.github.com>
2022-06-09 11:07:41 +0900

    Add files via upload

--- ofApp.cpp
+++ ofApp.cpp
@@ -3,8 +3,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	sound_F2.load("sounds/test.mp3");
-	sound_G2.load("sounds/ralo.mp3");
+	//sound_F2.load("sounds/sample.mp3");
 	
 	//sound_F2.setMultiPlay(true);
 	
@@ -29,7 +28,8 @@ void ofApp::setup(){
 	menu->CreateMenuFunction(&ofApp::appMenuFunction);
 
 	myFont1.loadFont("verdana.ttf", 12, true, true);
-	myFont2.loadFont("frabk.ttf", 12, true, true);
+	myFont_white.loadFont("frabk.ttf", 20, true, true);
+	myFont_black.loadFont("frabk.ttf", 15, true, true);
 
 	// window menu 생성
 	HMENU hMenu = menu->CreateWindowMenu();
@@ -59,12 +59,23 @@ void ofApp::setup(){
 
 	menu->SetWindowMenu();
 
-	white_keypressedflag = new int[14];
-	black_keypressedflag = new int[13];
+	// key part
+	num_of_white = 21;
+	num_of_black = 20;
+	white_keypressedflag = new int[num_of_white];
+	black_keypressedflag = new int[num_of_black];
+	for (int i = 0; i < num_of_white; i++) {
+		white_keypressedflag[i] = 0;
+	}
+	for (int i = 0; i < num_of_black; i++) {
+		black_keypressedflag[i] = 0;
+	}
 
 	// sound part;
 	//white_sound_flag = new int[14];
 	//black_sound_flag = new int[13];
+	sound_load();
+	
 	
 }
 
@@ -99,9 +110,11 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	
 	char str[200];
+	/*
 	// 건반의 길이를 설정
-	white_x = (float)((ofGetWidth()-20) / 21); // 좌우 여유 10px
+	white_x = (float)((ofGetWidth()-20) / num_of_white); // 좌우 여유 10px
 	white_y = (float)((ofGetHeight() - 20) / 2.5); // 하단 여유 20px
 	black_x = (float)white_x / 2;
 	black_y = (float)white_y * 3 / 5;
@@ -109,22 +122,23 @@ void ofApp::draw(){
 	white_r = white_x / 3;
 	black_r = black_x / 3;
 
-	float x1 = 10; 
-	float x2 = ofGetWidth() - 10;
-	float y2 = ofGetHeight() - 20;
-	float y1 = y2 - white_y;
+	x1 = 10; 
+	x2 = ofGetWidth() - 10;
+	y2 = ofGetHeight() - 20;
+	y1 = y2 - white_y;
 	ofSetColor(ofColor::black);
 	ofSetRectMode(OF_RECTMODE_CORNER);
 	ofSetLineWidth(5);
 	ofNoFill();
+	// draw the overall rectangle
 	ofDrawRectangle(x1, y1, x2 - x1-10, y2 - y1); // 모양 맞추기 위한 -10
 	float tempx = x1 + white_x;
-	// white key
-	for (int i = 0; i < 20; i++) {
+	// draw the white key
+	for (int i = 0; i < num_of_white-1; i++) {
 		ofDrawLine(tempx, y1, tempx, y2);
 		tempx += white_x;
 	}
-
+	// draw the black key
 	tempx = x1 + white_x * 3 / 4;
 	ofFill();
 	for (int i = 0; i < 3; i++) {
@@ -137,15 +151,44 @@ void ofApp::draw(){
 			tempx += white_x;
 		}
 	}
+	*/
 
+	if (is_intro) piano_draw();
+	
+	// if key pressed, then the window highlight that key
 	if (white_keypressedflag[F2] == 1) {
 		ofSetColor(ofColor::lightSalmon);
-		ofDrawCircle(x1 + white_x / 2, y2 - white_y / 5, white_r);
+		ofDrawCircle(x1 + white_x / 2, y2 - white_y / 6, white_r);
+	}
+	ofSetColor(ofColor::lightSalmon);
+	for (int i = 0; i < num_of_white; i++) {
+		if (white_keypressedflag[i] == 1) {
+			ofDrawCircle(x1 + white_x / 2 + i * white_x, y2 - white_y / 6, white_r);
+		}
 	}
 
-	ofSetColor(ofColor::gray);
+	for (int i = 0; i < num_of_black; i++) {
+		if (black_keypressedflag[i] == 1) {
+			ofDrawCircle(x1 + white_x * (i + 1), y2 - white_y / 2 - white_y / 60, black_r);
+		}
+	}
+
+	ofSetColor(ofColor::darkGray);
 	sprintf(str, " 20181277_ShinJaeyo_comsil_final_project ");
 	myFont1.drawString(str, 15, ofGetHeight() - 5);
+
+	ofSetColor(ofColor::black);
+	// print the white key name
+	for (int i = 0; i < num_of_white; i++) {
+		//sprintf(str, white_key_name[i]);
+		myFont_white.drawString(white_key_name[i], x1 + white_x * 1 / 3 + i * white_x, y2 - white_y / 7);
+	}
+	ofSetColor(ofColor::white);
+	// print the black key name
+	for (int i = 0; i < num_of_black; i++) {
+		//sprintf(str, black_key_name[i]);
+		myFont_black.drawString(black_key_name[i], x1 + white_x * 13 / 16 + i * white_x, y2 - white_y / 2);
+	}
 
 }
 
@@ -160,6 +203,7 @@ void ofApp::keyPressed(int key){
 		}
 		else {
 			ofExit();
+			free_Memory();
 		}
 	}
 	/*
@@ -171,34 +215,95 @@ void ofApp::keyPressed(int key){
 	}
 	*/
 
-	if (key == 'z' || key == 'Z') {
+	/*
+	if (key == 'z') {
 		if (white_keypressedflag[F2] != 1) {
 			white_keypressedflag[F2] = 1;
-			sound_F2.play();
+			white_sound[F2].play();
 		}
 	}
-	if (key == 'x' || key == 'X') {
+	if (key == 'x') {
 		if (white_keypressedflag[G2] != 1) {
 			white_keypressedflag[G2] = 1;
-			sound_G2.play();
+			white_sound[G2].play();
 		}
 	}
+
+	if (key == 'c') {
+		if (white_keypressedflag[A2] != 1) {
+			white_keypressedflag[A2] = 1;
+			white_sound[A2].play();
+		}
+	}
+
+	if (key == 'v') {
+		if (white_keypressedflag[B2] != 1) {
+			white_keypressedflag[B2] = 1;
+			white_sound[B2].play();
+		}
+	}
+	*/
+	for (int i = 0; i < num_of_white; i++) {
+		if (key == white_keyboard[i] && white_keypressedflag[i] != 1) {
+			white_keypressedflag[i] = 1;
+			white_sound[i].play();
+		}
+	}
+
+	for (int i = 0; i < num_of_black; i++) {
+		if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
+		if (key == black_keyboard[i] && black_keypressedflag[i] != 1) {
+			black_keypressedflag[i] = 1;
+			black_sound[i].play();
+		}
+	}
+
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-	
-	if (key == 'z' || key == 'Z') {
+	/*
+	if (key == 'z') {
 		if (white_keypressedflag[F2] == 1) {
 			white_keypressedflag[F2] = 0;
-			sound_F2.stop();
+			white_sound[F2].stop();
 		}
 	}
 	
-	if (key == 'x' || key == 'X') {
+	if (key == 'x') {
 		if (white_keypressedflag[G2] == 1) {
 			white_keypressedflag[G2] = 0;
-			sound_G2.stop();
+			white_sound[G2].stop();
+		}
+	}
+
+	if (key == 'c') {
+		if (white_keypressedflag[A2] == 1) {
+			white_keypressedflag[A2] = 0;
+			white_sound[A2].stop();
+		}
+	}
+
+	if (key == 'v') {
+		if (white_keypressedflag[B2] == 1) {
+			white_keypressedflag[B2] = 0;
+			white_sound[B2].stop();
+		}
+	}
+	*/
+	for (int i = 0; i < num_of_white; i++) {
+		if (key == white_keyboard[i] && white_keypressedflag[i] == 1) {
+			white_keypressedflag[i] = 0;
+			white_sound[i].stop();
+		}
+	}
+	
+
+	for (int i = 0; i < num_of_black; i++) {
+		if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
+		if (key == black_keyboard[i] && black_keypressedflag[i] == 1) {
+			black_keypressedflag[i] = 0;
+			black_sound[i].stop();
 		}
 	}
 	
@@ -297,3 +402,65 @@ void ofApp::doTopmost(bool bTop)
 			SetWindowPos(hWndForeground, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 	}
 } // end doTopmost
+
+
+void ofApp::free_Memory() {
+	// free the allocated memory
+	delete[] white_keypressedflag;
+	delete[] black_keypressedflag;
+	delete[] white_sound;
+	delete[] black_sound;
+}
+
+void ofApp::piano_draw() {
+	char str[200];
+	// 건반의 길이를 설정
+	white_x = (float)((ofGetWidth() - 20) / num_of_white); // 좌우 여유 10px
+	white_y = (float)((ofGetHeight() - 20) / 2.5); // 하단 여유 20px
+	black_x = (float)white_x / 2;
+	black_y = (float)white_y * 3 / 5;
+	// 눌렸을 때 표시될 원을 그릴 반지름 설정
+	white_r = white_x / 3;
+	black_r = black_x / 3;
+
+	x1 = 10;
+	x2 = ofGetWidth() - 10;
+	y2 = ofGetHeight() - 20;
+	y1 = y2 - white_y;
+	ofSetColor(ofColor::black);
+	ofSetRectMode(OF_RECTMODE_CORNER);
+	ofSetLineWidth(5);
+	ofNoFill();
+	// draw the overall rectangle
+	ofDrawRectangle(x1, y1, x2 - x1 - 10, y2 - y1); // 모양 맞추기 위한 -10
+	float tempx = x1 + white_x;
+	// draw the white key
+	for (int i = 0; i < num_of_white - 1; i++) {
+		ofDrawLine(tempx, y1, tempx, y2);
+		tempx += white_x;
+	}
+	// draw the black key
+	tempx = x1 + white_x * 3 / 4;
+	ofFill();
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 7; j++) {
+			if (j == 3 || j == 6) {
+				tempx += white_x;
+				continue;
+			}
+			ofDrawRectangle(tempx, y1, black_x, black_y);
+			tempx += white_x;
+		}
+	}
+}
+
+bool ofApp::sound_load() {
+	intro.load("sounds/intro.mp3");
+	intro.play();
+	is_intro = 1;
+	white_sound = new ofSoundPlayer[num_of_white];
+	black_sound = new ofSoundPlayer[num_of_black];
+
+	white_sound[F2].load("sounds/sample.mp3");
+	return true;
+}
--- ofApp.h
+++ ofApp.h
@@ -3,8 +3,14 @@
 #include "ofMain.h"
 #include "ofxWinMenu.h"
 
-typedef enum {F2, G2, A2, B2, C3, D3, E3, F3, G3, A3, B3, C4, D4, E4} white_piano_key;
-typedef enum {Gb2, Ab2, Bb2, NOT1, Db3, Eb3, NOT2, Gb3, Ab3, Bb3, NOT3, Db4, Eb4} black_piano_key;
+typedef enum {F2, G2, A2, B2, C3, D3, E3, F3, G3, A3, B3, C4, D4, E4, F4, G4, A4, B4, C5, D5, E5} white_piano_key;
+typedef enum {Gb2, Ab2, Bb2, NOT1, Db3, Eb3, NOT2, Gb3, Ab3, Bb3, NOT3, Db4, Eb4, NOT4, Gb4, Ab4, Bb4, NOT5, Db5, Eb5} black_piano_key;
+
+static char* white_key_name[] = {"F2\n z", "G2\n x", "A2\n c", "B2\n v", "C3\n b", "D3\n n", "E3\n m", "F3\n ,", "G3\n .", "A3\n q", "B3\n w", "C4\n e", "D4\n r", "E4\n t", "F4\n y", "G4\n u", "A4\n i", "B4\n o", "C5\n p", "D5\n [", "E5\n ]"};
+static char* black_key_name[] = { "Gb2\n  s", "Ab2\n  d", "Bb2\n  f", "", "Db3\n  h", "Eb3\n  j", "", "Gb3\n  l", "Ab3\n  1", "Bb3\n  2", "", "Db4\n  4", "Eb4\n  5", "", "Gb4\n  7", "Ab4\n  8", "Bb4\n  9", "", "Db5\n  -", "Eb5\n  =" };
+
+static char white_keyboard[] = "zxcvbnm,.qwertyuiop[]";
+static char black_keyboard[] = "sdf hj l12 45 789 -=";
 
 class ofApp : public ofBaseApp{
 
@@ -24,13 +30,17 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+		void free_Memory();
 		
+		void piano_draw();
+
 		ofxWinMenu* menu; // Menu object
 		void appMenuFunction(string title, bool bChecked); // Menu return function
 
 		// Used by example app
 		ofTrueTypeFont myFont1;
-		ofTrueTypeFont myFont2;
+		ofTrueTypeFont myFont_white;
+		ofTrueTypeFont myFont_black;
 		ofImage myImage;
 		float windowWidth, windowHeight;
 		HWND hWnd; // Application window
@@ -44,11 +54,17 @@ class ofApp : public ofBaseApp{
 		void doFullScreen(bool bFull);
 		void doTopmost(bool bTop);
 
+		int is_intro;
+
 		// 건반
+		int num_of_white;
+		int num_of_black;
 		float white_x; // 흰 건반의 폭
 		float white_y; // 흰 건반의 길이
 		float black_x; // 검은 폭
 		float black_y; // 검은 높이
+
+		float x1, x2, y1, y2; // 총 건반의 사각형 표시
 
 		int* white_keypressedflag; // key가 눌렸을 때, 화면에 표시하고 소리를 내는 등 flag
 		int* black_keypressedflag;
@@ -61,8 +77,11 @@ class ofApp : public ofBaseApp{
 		// Sound
 		int* white_sound_flag;
 		int* black_sound_flag;
-		ofSoundPlayer sound_F2;
-		ofSoundPlayer  beats;
-		ofSoundPlayer  sound_G2;
-		ofSoundPlayer  vocals;
+		//ofSoundPlayer sound_F2;
+		//ofSoundPlayer  sound_G2;
+
+		bool sound_load();
+		ofSoundPlayer intro;
+		ofSoundPlayer* white_sound;
+		ofSoundPlayer* black_sound;
 };
