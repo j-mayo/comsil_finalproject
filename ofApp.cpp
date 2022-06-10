4f7f0ddadfa052ec377cabb71ec3e7e0fdaeb770
j-mayo <86545225+j-mayo@users.noreply.github.com>
2022-06-10 16:02:16 +0900

    update

--- ofApp.cpp
+++ ofApp.cpp
@@ -7,9 +7,9 @@ void ofApp::setup(){
 	
 	//sound_F2.setMultiPlay(true);
 	
-
+	// 기본 설정. frame rate, title, 색, 메뉴 등을 설정해준다.
 	ofSetWindowTitle("Piano Playtime!");
-	ofSetFrameRate(15);
+	ofSetFrameRate(30);
 	ofBackground(255, 255, 255);
 
 	windowWidth = ofGetWidth();
@@ -27,6 +27,8 @@ void ofApp::setup(){
 
 	menu->CreateMenuFunction(&ofApp::appMenuFunction);
 
+
+	// 앞으로 사용할 font를 load
 	myFont1.loadFont("verdana.ttf", 12, true, true);
 	myFont_white.loadFont("frabk.ttf", 20, true, true);
 	myFont_black.loadFont("frabk.ttf", 15, true, true);
@@ -39,10 +41,10 @@ void ofApp::setup(){
 	menu->AddPopupSeparator(hPopup);
 	menu->AddPopupItem(hPopup, "Exit", false, false);
 
-	// 악기 메뉴
-	hPopup = menu->AddPopupMenu(hMenu, "Instrument");
-	menu->AddPopupItem(hPopup, "Piano", false, false);
-	menu->AddPopupItem(hPopup, "Violin", false, false);
+	// mode 메뉴
+	hPopup = menu->AddPopupMenu(hMenu, "Mode");
+	menu->AddPopupItem(hPopup, "Play Piano", false, false);
+	// menu->AddPopupItem(hPopup, "Violin", false, false);
 
 	// Mode ( if possible )
 	/*
@@ -58,25 +60,38 @@ void ofApp::setup(){
 	menu->AddPopupItem(hPopup, "About", false, false);
 
 	menu->SetWindowMenu();
+	
 
+	// 처음엔 기본적으로 play_piano 모드로 시작한다.
+	play_piano = 1;
 	// key part
+	// 현재 건반의 개수는 흰색 21개, 검은색 15개로 총 36개이다. 왜 검은색이 20개이냐, 그것은 바로 검은 건반은 연속하지 않으므로 구현 시의 효율성을 위해 20개로 했다.
 	num_of_white = 21;
 	num_of_black = 20;
+	// 필요한 자료구조들을 동적 할당 해 준다.
 	white_keypressedflag = new int[num_of_white];
 	black_keypressedflag = new int[num_of_black];
+	white_sustainflag = new int[num_of_white];
+	black_sustainflag = new int[num_of_black];
+	// 자료구조를 초기화한다.
 	for (int i = 0; i < num_of_white; i++) {
 		white_keypressedflag[i] = 0;
+		white_sustainflag[i] = 0;
 	}
 	for (int i = 0; i < num_of_black; i++) {
 		black_keypressedflag[i] = 0;
+		black_sustainflag[i] = 0;
 	}
+	// sustain_pedal은 초기에 눌리지 않으므로 0으로 초기화.
+	sustain_pedal = 0;
 
 	// sound part;
 	//white_sound_flag = new int[14];
 	//black_sound_flag = new int[13];
 	sound_load();
 	
-	
+	// image part
+	//display.load("images/musicimage.jpg");
 }
 
 void ofApp::appMenuFunction(string title, bool bChecked) {
@@ -98,8 +113,12 @@ void ofApp::appMenuFunction(string title, bool bChecked) {
 	//
 
 	if (title == "About") {
+		// 설명을 붙인다. 일단 아무 말이나 쓰긴 했는데 그렇게 중요한 부분도 아니니 몇 마디만 썼다.
 		ofSystemAlertDialog("Piano Playtime! Enjoy your own keyboard-piano!\nMade by SJY\n\nmofxWinMenu\nbasic example\n\nhttp://spout.zeal.co");
 	}
+
+	if (title == "Play Piano") play_piano = 1; // 기본적으로 1로 설정되어 있긴 하다.
+	// mode에 따라 추가 예정
 
 } // end appMenuFunction
 
@@ -111,7 +130,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 	
-	char str[200];
+	char str[200]; // myfont1에서 화면에 문장을 나타내기 위한 str 버퍼이다
 	/*
 	// 건반의 길이를 설정
 	white_x = (float)((ofGetWidth()-20) / num_of_white); // 좌우 여유 10px
@@ -152,42 +171,58 @@ void ofApp::draw(){
 		}
 	}
 	*/
-
-	if (is_intro) piano_draw();
+	//display.draw(ofGetWidth() / 6, 40, ofGetWidth() * 2 / 3, ofGetHeight() / 2);
+	piano_draw();
+	// 가장 먼저 피아노를 그려야 한다.
 	
 	// if key pressed, then the window highlight that key
+	/*
 	if (white_keypressedflag[F2] == 1) {
 		ofSetColor(ofColor::lightSalmon);
 		ofDrawCircle(x1 + white_x / 2, y2 - white_y / 6, white_r);
 	}
+	*/
+
+	// 눌린 건반에 색을 칠해 사용자가 눌렸음을 알 수 있게 한다.
+	ofFill();
 	ofSetColor(ofColor::lightSalmon);
-	for (int i = 0; i < num_of_white; i++) {
+	for (int i = 0; i < num_of_white; i++) { // 흰 건반에 대함
 		if (white_keypressedflag[i] == 1) {
-			ofDrawCircle(x1 + white_x / 2 + i * white_x, y2 - white_y / 6, white_r);
+			ofDrawCircle(x1 + white_x / 2 + i * white_x, y2 - white_y / 6, white_r); // 적절히 좌표를 계산해 보기 좋게 출력한다.
 		}
 	}
 
-	for (int i = 0; i < num_of_black; i++) {
-		if (black_keypressedflag[i] == 1) {
-			ofDrawCircle(x1 + white_x * (i + 1), y2 - white_y / 2 - white_y / 60, black_r);
+	for (int i = 0; i < num_of_black; i++) { // 검은 건반에 대함
+		if (black_keypressedflag[i] == 1) { // black_key의 경우 중간중간 없는 건반이 있지만 이것들은 절대 눌릴 염려가 없어 따로 처리하지 않았다.
+			ofDrawCircle(x1 + white_x * (i + 1), y2 - white_y / 2 - white_y / 60, black_r); // 적절히 좌표를 계산해 보기 좋게 출력한다.
 		}
 	}
+	if (sustain_pedal) ofDrawRectangle(ofGetWidth() / 2 - 2 * white_x, y2 + 25, 4 * white_x, 50); // sustain pedal의 경우 사각형을 표시했다ㅏ.
 
+
+	// print the string in window
+	// 글자는 항상 마지막에 draw되어야 사용자가 볼 수 있다.
+	
 	ofSetColor(ofColor::darkGray);
 	sprintf(str, " 20181277_ShinJaeyo_comsil_final_project ");
 	myFont1.drawString(str, 15, ofGetHeight() - 5);
 
+	// 흰 건반에 글자를 출력
 	ofSetColor(ofColor::black);
 	// print the white key name
 	for (int i = 0; i < num_of_white; i++) {
 		//sprintf(str, white_key_name[i]);
 		myFont_white.drawString(white_key_name[i], x1 + white_x * 1 / 3 + i * white_x, y2 - white_y / 7);
+		// 헤더에서 선언한 white_key_name으로 for문을 사용해 효율적으로 글자를 print한다.
 	}
+	myFont_white.drawString("Sustain pedal(Space bar)", ofGetWidth() / 2 - 1.5 * white_x, ofGetHeight() - 60);
 	ofSetColor(ofColor::white);
 	// print the black key name
 	for (int i = 0; i < num_of_black; i++) {
 		//sprintf(str, black_key_name[i]);
 		myFont_black.drawString(black_key_name[i], x1 + white_x * 13 / 16 + i * white_x, y2 - white_y / 2);
+		// 헤더에서 선언한 black_key_name으로 for문을 사용해 효율적으로 글자를 print한다.
+		// 검은 건반이 없는 위치의 black_key_name은 빈 문자열이니 아무런 문제가 없다. 하나의 for문으로 깔끔하게 구현이 가능했다.
 	}
 
 }
@@ -195,6 +230,7 @@ void ofApp::draw(){
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 	// full screen 에 대한 부분!
+	printf("%c\n", key);
 	if (key == VK_ESCAPE) {
 		// Disable fullscreen set, otherwise quit the application as usual
 		if (bFullscreen) {
@@ -242,9 +278,34 @@ void ofApp::keyPressed(int key){
 			white_sound[B2].play();
 		}
 	}
+	// test!!!
 	*/
+
+	if (key == ' ') { // sustain pedal이 눌린 경우, 현재 눌린 음을 계속 지속해야 한다. 이를 위해 따로 표시한다.
+		if (play_piano) sustain_pedal = 1;
+		
+	}
+
+	// sustain pedal이 눌린 경우, 이 항목을 실행한다.
+	if (sustain_pedal == 1) {
+		// 현재 눌린 건반들에 대해, sustain_flag를 1로 설정해 눌려있다는 것을 알린다.
+		for (int i = 0; i < num_of_white; i++) {
+			if (white_keypressedflag[i] == 1) {
+				white_sustainflag[i] = 1;
+			}
+		}
+		for (int i = 0; i < num_of_black; i++) {
+			// if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
+			if (black_keypressedflag[i] == 1) {
+				black_sustainflag[i] = 1;
+			}
+		}
+	}
+
 	for (int i = 0; i < num_of_white; i++) {
-		if (key == white_keyboard[i] && white_keypressedflag[i] != 1) {
+		// 음 재생의 조건은 현재 play_piano mode이고, 입력으로 들어온 key가 눌리지 않은 상태라면 음을 재생한다.
+		// 설정에 따라 달라질 수 있기는 하나 이미 눌린 상태에서 음을 재생 시 중첩되어 귀가 아플 수 있다
+		if (play_piano && key == white_keyboard[i] && white_keypressedflag[i] != 1) {
 			white_keypressedflag[i] = 1;
 			white_sound[i].play();
 		}
@@ -252,16 +313,20 @@ void ofApp::keyPressed(int key){
 
 	for (int i = 0; i < num_of_black; i++) {
 		if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
-		if (key == black_keyboard[i] && black_keypressedflag[i] != 1) {
+		// 검은 건반이 없는 위치에 대해서는 key 입력을 받지 않아야 한다.
+		if (play_piano && key == black_keyboard[i] && black_keypressedflag[i] != 1) {
 			black_keypressedflag[i] = 1;
 			black_sound[i].play();
 		}
 	}
 
+	
+
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+
 	/*
 	if (key == 'z') {
 		if (white_keypressedflag[F2] == 1) {
@@ -291,19 +356,43 @@ void ofApp::keyReleased(int key){
 		}
 	}
 	*/
-	for (int i = 0; i < num_of_white; i++) {
-		if (key == white_keyboard[i] && white_keypressedflag[i] == 1) {
-			white_keypressedflag[i] = 0;
-			white_sound[i].stop();
+	if (key == ' ') {
+		// sustain pedal에서 손을 뗀 경우, pedal로 인해 지속 중인 모든 음을 멈춰야 한다.
+		sustain_pedal = 0;
+		// 스페이스바를 떼면 sustain중이였던 모든 key들이 눌리지 않은 상태로 변한다.
+		for (int i = 0; i < num_of_white; i++) {
+			if (play_piano && white_sustainflag[i] == 1) {
+				white_keypressedflag[i] = 0;
+				white_sustainflag[i] = 0;
+				// white_sound[i].stop();
+				
+			}
+		}
+
+		for (int i = 0; i < num_of_black; i++) {
+			if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
+			if (play_piano && black_sustainflag[i] == 1) {
+				black_keypressedflag[i] = 0;
+				black_sustainflag[i] = 0;
+				// black_sound[i].stop();
+			}
 		}
 	}
-	
+	if (play_piano && sustain_pedal == 0) {
+		// sustain_pedal이 눌린 상태라면 음을 종료해서는 안 되므로 조건에 추가한다.
+		for (int i = 0; i < num_of_white; i++) {
+			if (key == white_keyboard[i] && white_keypressedflag[i] == 1) {
+				white_keypressedflag[i] = 0;
+				white_sound[i].stop();
+			}
+		}
 
-	for (int i = 0; i < num_of_black; i++) {
-		if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
-		if (key == black_keyboard[i] && black_keypressedflag[i] == 1) {
-			black_keypressedflag[i] = 0;
-			black_sound[i].stop();
+		for (int i = 0; i < num_of_black; i++) {
+			if (i == 3 || i == 6 || i == 10 || i == 13 || i == 17) continue;
+			if (key == black_keyboard[i] && black_keypressedflag[i] == 1) {
+				black_keypressedflag[i] = 0;
+				black_sound[i].stop();
+			}
 		}
 	}
 	
@@ -408,13 +497,16 @@ void ofApp::free_Memory() {
 	// free the allocated memory
 	delete[] white_keypressedflag;
 	delete[] black_keypressedflag;
+	delete[] white_sustainflag;
+	delete[] black_sustainflag;
 	delete[] white_sound;
 	delete[] black_sound;
 }
 
 void ofApp::piano_draw() {
 	char str[200];
-	// 건반의 길이를 설정
+
+	// 화면에 나올 건반 길이를 설정
 	white_x = (float)((ofGetWidth() - 20) / num_of_white); // 좌우 여유 10px
 	white_y = (float)((ofGetHeight() - 20) / 2.5); // 하단 여유 20px
 	black_x = (float)white_x / 2;
@@ -423,23 +515,24 @@ void ofApp::piano_draw() {
 	white_r = white_x / 3;
 	black_r = black_x / 3;
 
+	// 피아노 전체 사각형 꼭짓점 설정
 	x1 = 10;
 	x2 = ofGetWidth() - 10;
-	y2 = ofGetHeight() - 20;
+	y2 = ofGetHeight() - 100 - 20;
 	y1 = y2 - white_y;
 	ofSetColor(ofColor::black);
 	ofSetRectMode(OF_RECTMODE_CORNER);
 	ofSetLineWidth(5);
 	ofNoFill();
-	// draw the overall rectangle
+	// 전체적인 틀 사각형 그림
 	ofDrawRectangle(x1, y1, x2 - x1 - 10, y2 - y1); // 모양 맞추기 위한 -10
 	float tempx = x1 + white_x;
-	// draw the white key
+	// 흰 건반 그림
 	for (int i = 0; i < num_of_white - 1; i++) {
 		ofDrawLine(tempx, y1, tempx, y2);
 		tempx += white_x;
 	}
-	// draw the black key
+	// 검은 건반 그림
 	tempx = x1 + white_x * 3 / 4;
 	ofFill();
 	for (int i = 0; i < 3; i++) {
@@ -452,17 +545,26 @@ void ofApp::piano_draw() {
 			tempx += white_x;
 		}
 	}
+	// draw the space bar(sustain pedal을 그린 것)
+	ofNoFill();
+	ofDrawRectangle(ofGetWidth() / 2 - 3 * white_x, y2 + 20, 6 * white_x, 70);
 }
 
 bool ofApp::sound_load() {
-	intro.load("sounds/intro.mp3");
+	// 필요한 sound file을 load하는 함수
+	is_intro = intro.load("sounds/intro.mp3"); // loading 성공 시 1, 아니면 0
 	intro.play();
-	is_intro = 1;
+
+
+	// 배열 할당
 	white_sound = new ofSoundPlayer[num_of_white];
 	black_sound = new ofSoundPlayer[num_of_black];
-	char extension[] = ".mp3";
+	// 파일 확장자 설정. 필요에 의해 바꿀 수 있다.
+	char extension[] = ".wav";
+	// 건반 이름과 키를 화면에 출력할 때 쓰는 문자열 버퍼
 	char* temps = (char*)malloc(sizeof(int) * 20);
 	
+	// 헤더에서 선언한 white_file_name, black_file_name과 strcpy, strcat 함수로 경로를 인자로 전달해 wav 파일을 load한다.
 	for (int i = 0; i < num_of_white; i++) {
 		strcpy(temps, "sounds/");
 		strcat(temps, white_file_name[i]);
--- ofApp.h
+++ ofApp.h
@@ -6,17 +6,21 @@
 
 typedef enum {F2, G2, A2, B2, C3, D3, E3, F3, G3, A3, B3, C4, D4, E4, F4, G4, A4, B4, C5, D5, E5} white_piano_key;
 typedef enum {Gb2, Ab2, Bb2, NOT1, Db3, Eb3, NOT2, Gb3, Ab3, Bb3, NOT3, Db4, Eb4, NOT4, Gb4, Ab4, Bb4, NOT5, Db5, Eb5} black_piano_key;
+// enum type 구조를 활용해 코드의 가독성을 높이고 구현을 용이하게 하였다.
 
 static char* white_key_name[] = {"F2\n z", "G2\n x", "A2\n c", "B2\n v", "C3\n b", "D3\n n", "E3\n m", "F3\n ,", "G3\n .", "A3\n q", "B3\n w", "C4\n e", "D4\n r", "E4\n t", "F4\n y", "G4\n u", "A4\n i", "B4\n o", "C5\n p", "D5\n [", "E5\n ]"};
 static char* black_key_name[] = { "Gb2\n  s", "Ab2\n  d", "Bb2\n  f", "", "Db3\n  h", "Eb3\n  j", "", "Gb3\n  l", "Ab3\n  1", "Bb3\n  2", "", "Db4\n  4", "Eb4\n  5", "", "Gb4\n  7", "Ab4\n  8", "Bb4\n  9", "", "Db5\n  -", "Eb5\n  =" };
-
+// 건반에 직접 출력될 문자열이다. 각 건반마다 해당하는 음과, 눌러야 할 키를 같이 명시했다. 검은 건반의 경우 중간중간 건너뛰어야 하므로 빈 문자열을 집어넣었다.
 
 static char* white_file_name[] = {"F2", "G2", "A2", "B2", "C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5"};
 static char* black_file_name[] = { "Gb2", "Ab2", "Bb2", "", "Db3", "Eb3", "", "Gb3", "Ab3", "Bb3", "", "Db4", "Eb4", "", "Gb4", "Ab4", "Bb4", "", "Db5", "Eb5"};
-
+// 각 음에 해당하는 sound file을 불러올 때 코드의 양을 줄이기 위해 선언하였다. enum type을 활용해 (ex: white_file_name[F2] = "F2") 해당하는 음의 파일명을 알 수 있다
 
 static char white_keyboard[] = "zxcvbnm,.qwertyuiop[]";
 static char black_keyboard[] = "sdf hj l12 45 789 -=";
+// key 입력이 들어왔을 때 건반에 일대일 대응되는 모든 키를 확인해야 하는데, 일일히 for문을 달아주면 코드 양이 끔찍하게 불어날 것이다.
+// 이를 위해 미리 각 키들의 문자열 배열을 선언해, 맨 위의 enum type과 함께 (if key == white_keyboard[F2]) 등으로 키 입력을 알 수 있고,
+// enum type이므로 for loop로 간결하게 key의 입력을 검사할 수 있다는 장점이 있다.
 
 class ofApp : public ofBaseApp{
 
@@ -36,19 +40,20 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		void free_Memory();
+
+		// 추가 선언한 함수들
+		void free_Memory(); // 사용된 동적 메모리를 해제하는 함수
 		
-		void piano_draw();
+		void piano_draw(); // 피아노 건반들을 화면에 그려주는 역할.
+
+		bool sound_load(); // 필요한 sound file들을 로딩해주는 역할
 
 		ofxWinMenu* menu; // Menu object
 		void appMenuFunction(string title, bool bChecked); // Menu return function
 
-		// Used by example app
-		ofTrueTypeFont myFont1;
-		ofTrueTypeFont myFont_white;
-		ofTrueTypeFont myFont_black;
-		ofImage myImage;
-		float windowWidth, windowHeight;
+		
+
+		float windowWidth, windowHeight; 
 		HWND hWnd; // Application window
 		HWND hWndForeground; // current foreground window
 
@@ -60,34 +65,48 @@ class ofApp : public ofBaseApp{
 		void doFullScreen(bool bFull);
 		void doTopmost(bool bTop);
 
-		int is_intro;
+		// 추가 선언해준 변수들
+		ofTrueTypeFont myFont1; // consil... 출력 위한 폰트
+		ofTrueTypeFont myFont_white; // 흰 건반의 음과 눌러야 하는 키보드 버튼을 출력해주기 위한 폰트
+		ofTrueTypeFont myFont_black; // 검은 건반의 음과 눌러야 하는 키보드 버튼을 출력해주기 위한 폰트
+		ofImage myImage; // image를 넣을 일이 있을 때 사용,  *** 없으면 삭제 예정 ***
+
+		int is_intro; // intro 음이 loading되었는지 알려주는 변수. 1이면 로딩 된 것.
+		int play_piano; // play_piano mode인지 알려주는 변수. 1이면 play_piano인 것
 
 		// 건반
-		int num_of_white;
-		int num_of_black;
+		int num_of_white; // 흰 건반의 개수
+		int num_of_black; // 검은 건반의 개수
 		float white_x; // 흰 건반의 폭
 		float white_y; // 흰 건반의 길이
 		float black_x; // 검은 폭
 		float black_y; // 검은 높이
 
-		float x1, x2, y1, y2; // 총 건반의 사각형 표시
+		float x1, x2, y1, y2; // 총 건반의 사각형 표시, 왼쪽 위와 오른쪽 아래 꼭짓점의 좌효
 
 		int* white_keypressedflag; // key가 눌렸을 때, 화면에 표시하고 소리를 내는 등 flag
-		int* black_keypressedflag;
-		int white_r;
-		int black_r;
-		int* white_circle_x;
-		int* black_circle_x;
+		int* black_keypressedflag; // (black key의 경우)
+		int white_r; // key가 눌렸을 때 화면에 키가 눌림을 표시해주는 원을 그릴 때의 반지름
+		int black_r; // (black key의 경우)
+		//int* white_circle_x; 
+		//int* black_circle_x;
+
+		int* white_sustainflag; // 현재 sustain중인 흰 건반들을 표시해주는 flag 배열
+		int* black_sustainflag; // (black key의 경우)
+		int sustain_pedal; // sustain pedal이 눌린 경우 1, 아니면 0
 
 
 		// Sound
-		int* white_sound_flag;
-		int* black_sound_flag;
+		//int* white_sound_flag; 
+		//int* black_sound_flag;
 		//ofSoundPlayer sound_F2;
 		//ofSoundPlayer  sound_G2;
 
-		bool sound_load();
-		ofSoundPlayer intro;
-		ofSoundPlayer* white_sound;
-		ofSoundPlayer* black_sound;
+		
+		ofSoundPlayer intro; // intro 음악을 담을 ofSoundPlayer 객체
+		ofSoundPlayer* white_sound; // 흰 건반의 음들을 담는 ofSoundPlayer의 배열
+		ofSoundPlayer* black_sound; // 검은 건반의 음들을 담는 ofSoundPlayer의 배열
+
+		// 배경
+		ofImage display; // 배경 사진을 그리는 데 이용하는 ofImage 타입 객체, *** 이용하지 않을 시 삭제 예정 ***
 };
